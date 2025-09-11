e37b6f99254bd7dd86d20bc09622458710bdc8a1
Pedro Rodrigues Almeida <pedrominato23@gmail.com>
2025-09-11 09:27:07 -0300

    Criando validacao de dados da autenticacao

--- app.js
+++ app.js
@@ -6,11 +6,57 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
-// Rota publica 
+//Config json
+app.use(express.json());
 
+//importando models
+const User =require('./models/User')
+
+// Rota publica 
 app.get('/', (req, res) => {
     res.status(200).json({msg: "Bem vindo ao Passa Bola"})
 })
+
+//registrando usuario 
+app.post('/auth/register', async (req, res)=> {
+
+    const {name, email, senha, confirmasenha} = req.body
+    //VALIDACAO
+       // VALIDACAO - Verifique todos os campos
+    if (!name) {
+        return res.status(422).json({msg: 'O nome é obrigatório'});
+    }
+    if (!email) {
+        return res.status(422).json({msg: 'O email é obrigatório'});
+    }
+    if (!senha) {
+        return res.status(422).json({msg: 'A senha é obrigatória'});
+    }
+
+    if (senha !== confirmasenha) {
+        return res.status(422).json({msg: 'As senhas não conferem'});
+    }
+    // Verificando se o usuario exists
+    const userExistente = await User.findOne({email: email})
+
+    if(userExistente){
+        return res.status(422).json({msg: 'Por favor, ultilize outro e-mail!'})
+    }
+    //Criando password 
+    const salt = await bcrypt.genSalt(12)
+    const passsWordHash = await bcrypt.hash(senha, salt)
+
+    //Criando usuario 
+    const user = new User({
+        name,
+        email,
+        senha,
+    })
+
+    // Se todas as validações passarem, envie uma resposta de sucesso
+    // Isso é temporário, apenas para você ver que funcionou.
+    return res.status(200).json({msg: 'Usuário validado com sucesso!'});
+});
 
 //Credenciais
 const dbUser = process.env.DB_USER
@@ -24,5 +70,5 @@ mongooso
     })
     .catch((err) => console.log(err))
 
-app.listen(3000)
+
 
---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose =require(`mongoose`)    
+
+const user = mongoose.model(`User`, {
+    name: String,
+    email: String,
+    senha: String
+})
+
+module.exports = user
